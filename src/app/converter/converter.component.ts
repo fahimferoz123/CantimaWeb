ca157339e188d7057a87c4ec132294035a8fe3e8
dulaj dhanushanka <dulajdhanushanka@gmail.com>
2020-10-14 13:41:12 +0530

    credits

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConverterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  userId = "";
+  amount = 0;
 
   ngOnInit(): void {
   }
 
+
+  addCredits() {
+    var url = "http://3.223.72.19/api/order/add-credits"
+    this.http.post(url, {
+      "id": this.userId,
+      "amount": {
+        "amount": this.amount
+      }
+    }, { observe: 'response' }
+    ).subscribe({
+      next: data => {
+        console.log(data);
+
+        if (data.status === 200) {
+          // this.router.navigateByUrl('/instock');
+          console.log("credits added");
+
+        } else {
+          // add a error message here
+          console.log("error occured");
+
+        }
+      },
+      error: error => {
+        // this.errorMessage = error.message;
+        console.error('There was an error!', error);
+        // add a error message here
+
+      }
+    }
+    );
+
+  }
 }
--- src/app/showcase/showcase.component.ts
+++ src/app/showcase/showcase.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowcaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.getTodaydata();
+  }
 
   ngOnInit(): void {
   }
 
+  getTodaydata() {
+    this.http.get('http://3.223.72.19/api/history/today', { observe: 'response' }
+    ).subscribe({
+      next: data => {
+        console.log(data);
+
+        if (data.status === 200) {
+          // this.router.navigateByUrl('/instock');
+          console.log("today data fetched");
+
+        } else {
+          // add a error message here
+          console.log("error occured");
+
+        }
+      },
+      error: error => {
+        // this.errorMessage = error.message;
+        console.error('There was an error!', error);
+        // add a error message here
+
+      }
+    }
+    );
+  }
 }
