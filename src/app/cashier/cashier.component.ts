d9305c3a2aa519dbbe73191321124cc765fce517
dulaj dhanushanka <dulajdhanushanka@gmail.com>
2020-10-14 14:12:10 +0530

    order

--- src/app/cashier/cashier.component.ts
+++ src/app/cashier/cashier.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 
@@ -8,9 +9,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CashierComponent implements OnInit {
 
-  constructor() { }
+  orderId;
+
+
+  userid;
+  time;
+
+  allitems;
+  total;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  getOrderDetails() {
+
+    if (this.orderId != null) {
+      let url = "http://3.223.72.19/api/order/" + this.orderId;
+      this.http.get(url, { observe: 'response' }
+      ).subscribe({
+        next: data => {
+          console.log(data.body['order']);
+
+          if (data.status === 200) {
+            // this.router.navigateByUrl('/instock');
+            console.log("order data fetched");
+
+            this.userid = data.body['order']['customerID'];
+            this.time = data.body['order']['date'];
+            this.allitems = data.body['order']['items'];
+            this.total = data.body['order']['price'];
+
+          } else {
+            // add a error message here
+            console.log("error occured");
+
+          }
+        },
+        error: error => {
+          // this.errorMessage = error.message;
+          console.error('There was an error!', error);
+          // add a error message here
+
+        }
+      }
+      );
+
+    } else {
+
+    }
+  }
+
+
+
+  completeOrder() {
+
+  }
 }
